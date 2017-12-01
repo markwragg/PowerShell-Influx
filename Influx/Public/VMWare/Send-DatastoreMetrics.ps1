fb44db7a2d5445120e90e55a129e5afe4ca8b0a9
markwragg <mark.wragg@gmail.com>
2017-12-01 08:50:40 +0000

    Adding state

--- Influx/Public/VMWare/Send-DatastoreMetrics.ps1
+++ Influx/Public/VMWare/Send-DatastoreMetrics.ps1
@@ -61,7 +61,7 @@
             }
 
             $Metrics = @{
-                State = "'$($DS.State)'"
+                State = '"' + $DS.State + '"'
                 CapacityGB = $DS.CapacityGB
                 FreeSpaceGB = $DS.FreeSpaceGB
                 UsedSpaceGB = ($DS.CapacityGB - $DS.FreeSpaceGB)
