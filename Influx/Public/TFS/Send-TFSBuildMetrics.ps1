5ee0e07f2b9b157738d47a6d98076812a324e9e9
markwragg <mark.wragg@gmail.com>
2017-12-12 10:57:26 +0000

    bug

--- Influx/Public/TFS/Send-TFSBuildMetrics.ps1
+++ Influx/Public/TFS/Send-TFSBuildMetrics.ps1
@@ -84,7 +84,7 @@
     }
     
     Write-Verbose 'Getting builds..'
-    $Builds = Get-TFSBuilds -Top $Top | Where-Object { $_.$Build.StartTime }
+    $Builds = Get-TFSBuilds -Top $Top | Where-Object { $_.StartTime }
 
     if ($Builds) {
     
