985f683c74a49ef893536efd1cbaab7153f88907
markwragg <mark.wragg@gmail.com>
2017-12-31 09:23:06 +0000

    CI/CD Pipeline

--- Tests/ConvertTo-UnixMillisecond.Tests.ps1
+++ Tests/ConvertTo-UnixMillisecond.Tests.ps1
@@ -2,7 +2,7 @@
 $sut = Get-ChildItem "$here\..\Influx\*.ps1" -Recurse -File
 $sut | ForEach-Object { . $_.FullName }
 
-Describe 'ConvertTo-UnixTimeMillisecond' {
+Describe 'ConvertTo-UnixTimeMillisecond Tests' {
     $UnixTime = Get-Date "01/01/2017" | ConvertTo-UnixTimeMillisecond
 
     It "Should convert 01/01/2017 to 1483228800000" {
---
+++ psake.ps1
@@ -0,0 +1,68 @@
+﻿# PSake makes variables declared here available in other scriptblocks
+# Init some things
+Properties {
+    # Find the build folder based on build system
+    $ProjectRoot = $ENV:BHProjectPath
+    if(-not $ProjectRoot)
+    {
+        $ProjectRoot = $PSScriptRoot
+    }
+
+    $Verbose = @{}
+    if($ENV:BHCommitMessage -match "!verbose")
+    {
+        $Verbose = @{Verbose = $True}
+    }
+}
+
+Task Default -Depends Deploy
+
+Task Init {
+    '----------------------------------------------------------------------'
+    Set-Location $ProjectRoot
+    "Build System Details:"
+    Get-Item ENV:BH*
+    "`n"
+}
+
+Task Test -Depends Init  {
+    '----------------------------------------------------------------------'
+    
+    $Timestamp = Get-date -uformat "%Y%m%d-%H%M%S"
+    $PSVersion = $PSVersionTable.PSVersion.Major
+    $TestFile = "TestResults_PS$PSVersion`_$TimeStamp.xml"
+        
+    "`n`tSTATUS: Testing with PowerShell $PSVersion"
+
+    # Gather test results. Store them in a variable and file
+    $TestResults = Invoke-Pester -Path $ProjectRoot\Tests -PassThru -OutputFormat NUnitXml -OutputFile "$ProjectRoot\$TestFile" -ExcludeTag Integration
+
+    # In Appveyor?  Upload our tests! #Abstract this into a function?
+    If($ENV:BHBuildSystem -eq 'AppVeyor')
+    {
+        (New-Object 'System.Net.WebClient').UploadFile(
+            "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)",
+            "$ProjectRoot\$TestFile" )
+    }
+
+    Remove-Item "$ProjectRoot\$TestFile" -Force -ErrorAction SilentlyContinue
+
+    # Failed tests?
+    # Need to tell psake or it will proceed to the deployment. Danger!
+    if($TestResults.FailedCount -gt 0)
+    {
+        Write-Error "Failed '$($TestResults.FailedCount)' tests, build failed"
+    }
+    "`n"
+}
+
+Task Build -Depends Test {
+    '----------------------------------------------------------------------'
+    #Set-ModuleFunctions
+}
+
+Task Deploy -Depends Build {
+    '----------------------------------------------------------------------'
+    #Not in use for this project yet
+
+}
