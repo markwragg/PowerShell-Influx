0736c1a4612bca41698bdf573dc09dd19637b111
markwragg <mark.wragg@gmail.com>
2018-01-01 22:17:31 +0000

    Pester tests

---
+++ Tests/ConvertTo-UnixTimeMillisecond.Tests.ps1
@@ -0,0 +1,12 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+$root = "$here\..\"
+Get-ChildItem $root -Recurse | Where-Object { $_.Name -eq $sut } | ForEach-Object { . $_.FullName }
+
+Describe 'ConvertTo-UnixTimeMillisecond' {
+    $UnixTime = Get-Date "01/01/2017" | ConvertTo-UnixTimeMillisecond
+
+    It "Should convert 01/01/2017 to 1483228800000" {
+        $UnixTime | Should Be 1483228800000
+    }
+}
---
+++ Tests/Out-InfluxEscapeString.Tests.ps1
@@ -0,0 +1,12 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+$root = "$here\..\"
+Get-ChildItem $root -Recurse | Where-Object { $_.Name -eq $sut } | ForEach-Object { . $_.FullName }
+
+Describe 'Out-InfluxEscapeString' {
+    $SomeString = 'Some ,string=' | Out-InfluxEscapeString
+
+    It "Should convert 'Some ,string=' to 'Some\ \,string\='" {
+        $SomeString | Should Be 'Some\ \,string\='
+    }
+}
