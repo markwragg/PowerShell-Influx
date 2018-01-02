46031472b817fadbd5684026cad0c04735b10056
markwragg <mark.wragg@gmail.com>
2018-01-02 16:35:35 +0000

    Pester

--- Tests/Write-Influx.Tests.ps1
+++ Tests/Write-Influx.Tests.ps1
@@ -2,17 +2,58 @@ if(-not $PSScriptRoot) { $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path
 
 $PSVersion = $PSVersionTable.PSVersion.Major
 $Root = "$PSScriptRoot\..\"
-$Sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 
-Get-ChildItem $Root -Filter $Sut -File -Recurse | ForEach-Object { . $_.FullName }
+Import-Module "$Root\Influx" -Force
 
 Describe "Write-Influx PS$PSVersion" {
     
-    Mock Write-Influx { $null }
+    InModuleScope Influx {
 
-    $WriteInflux = Write-Influx -Measure WebServer -Tags @{Server='Host01'} -Metrics @{CPU=100; Memory=50} -Database Web -Server http://myinflux.local:8086
+        Mock Out-InfluxEscapeString { 'Some\ \,string\=' } -Verifiable
+        Mock ConvertTo-UnixTimeNanosecond { '1483274062120000000' }
+        Mock Invoke-RestMethod { $null } -Verifiable
+        Mock Invoke-RestMethod { $null } -ParameterFilter {$WhatIf -eq $true}
+       
+        Context 'Simulating successful write' {
+           
+            $WriteInflux = Write-Influx -Measure WebServer -Tags @{Server='Host01'} -Metrics @{CPU=100; Memory=50} -Database Web -Server http://localhost:8086 -Timestamp (Get-Date)
 
-    It 'Write-Influx should return null' {
-        $WriteInflux | Should -Be $null
+            It 'Write-Influx should return null' {
+                $WriteInflux | Should -Be $null
+            }
+            It 'Should execute all verifiable mocks' {
+                Assert-VerifiableMock
+            }
+            It 'Should call ConvertTo-UnixTimeNanosecond exactly 1 time' {
+                Assert-MockCalled ConvertTo-UnixTimeNanosecond -Exactly 1
+            }
+            It 'Should call Out-InfluxEscapeString exactly 8 times' {
+                Assert-MockCalled Out-InfluxEscapeString -Exactly 8
+            }
+            It 'Should call Invoke-RestMethod exactly 1 time' {
+                Assert-MockCalled Invoke-RestMethod -Exactly 1
+            }
+        }
+
+        Context 'Simulating -WhatIf and no Timestamp specified' {
+            
+            $WriteInflux = Write-Influx -Measure WebServer -Tags @{Server='Host01'} -Metrics @{CPU=100; Memory=50} -Database Web -Server http://localhost:8086 -WhatIf
+
+            It 'Write-Influx should return null' {
+                $WriteInflux | Should -Be $null
+            }
+            It 'Should execute all verifiable mocks' {
+                Assert-VerifiableMock
+            }
+            It 'Should call Out-InfluxEscapeString exactly 8 times' {
+                Assert-MockCalled Out-InfluxEscapeString -Exactly 8
+            }
+            It 'Should call ConvertTo-UnixTimeNanosecond exactly 0 times' {
+                Assert-MockCalled ConvertTo-UnixTimeNanosecond -Exactly 0
+            }
+            It 'Should call Invoke-RestMethod exactly 0 times' {
+                Assert-MockCalled Invoke-RestMethod -Exactly 0
+            }
+        }
     }
 }
