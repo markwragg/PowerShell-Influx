b4ea4877e8d80627e4b937a146e0fc1cf8e46f99
markwragg <mark.wragg@gmail.com>
2018-01-02 12:34:09 +0000

    Pester

--- Tests/Write-Influx.Tests.ps1
+++ Tests/Write-Influx.Tests.ps1
@@ -4,12 +4,12 @@ $PSVersion = $PSVersionTable.PSVersion.Major
 $Root = "$PSScriptRoot\..\"
 $Sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 
-Get-ChildItem $Root -Filter $Sut -Recurse | ForEach-Object { . $_.FullName }
+Get-ChildItem $Root -Filter $Sut -File -Recurse | ForEach-Object { . $_.FullName }
 
 Describe "Write-Influx PS$PSVersion" {
-
-    Mock Write-Influx { }
     
+    Mock Write-Influx { $null }
+
     $WriteInflux = Write-Influx -Measure WebServer -Tags @{Server='Host01'} -Metrics @{CPU=100; Memory=50} -Database Web -Server http://myinflux.local:8086
 
     It 'Write-Influx should return null' {
