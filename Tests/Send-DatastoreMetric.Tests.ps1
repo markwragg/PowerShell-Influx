f48b22e1ed9abcf7df2702fd93730ffd3a5768ae
markwragg <mark.wragg@gmail.com>
2018-01-03 14:10:56 +0000

    VMWare datastore tests

---
+++ Tests/Send-DatastoreMetric.Tests.ps1
@@ -0,0 +1,61 @@
+if(-not $PSScriptRoot) { $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
+
+$PSVersion = $PSVersionTable.PSVersion.Major
+$Root = "$PSScriptRoot\.."
+$Module = 'Influx'
+
+If (-not (Get-Module $Module)) { Import-Module "$Root\$Module" -Force }
+
+Describe "Send-DatastoreMetric PS$PSVersion" {
+        
+    InModuleScope Influx {
+
+        Function Get-Datastore { }
+        
+        Mock Write-Influx { }
+
+        Context 'Simulating successful send' {
+            
+            Mock Get-Datastore { 
+                [PSCustomObject]@{ 
+                    Name = 'Test Datastore' 
+                    CapacityGB = 12345.987
+                    FreespaceGB = 654.123
+                }
+            } -Verifiable
+
+            $SendDatastore = Send-DatastoreMetric
+            
+            it 'Should return null' {
+                $SendDatastore | Should be $null
+            }
+            It 'Should execute all verifiable mocks' {
+                Assert-VerifiableMock
+            }
+            It 'Should call Get-Datastore exactly 1 time' {
+                Assert-MockCalled Get-Datastore -Exactly 1
+            }
+            It 'Should call Write-Influx exactly 1 time' {
+                Assert-MockCalled Write-Influx -Exactly 1
+            }  
+        }
+
+        Context 'Simulating no Datastore data returned' {
+        
+            Mock Get-Datastore { } -Verifiable
+
+            it 'Should throw if no Datacenter data returned' {
+                { Send-DatastoreMetric } | Should Throw 'No datastore data returned'
+            }  
+            It 'Should execute all verifiable mocks' {
+                Assert-VerifiableMock
+            }
+            It 'Should call Get-Datastore exactly 1 time' {
+                Assert-MockCalled Get-Datastore -Exactly 1
+            }
+            It 'Should call Write-Influx exactly 0 times' {
+                Assert-MockCalled Write-Influx -Exactly 0
+            }
+        }     
+    }
+}
